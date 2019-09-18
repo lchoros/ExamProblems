be82cf6e0f7dc949c059c2c9d5930ad00e809238
lukaszchoros <lukasz.choros@jpsfinancial.co.uk>
2019-09-18 16:58:29 +0100

    Perfect Diamond

--- IncreasingElements/Program.cs
+++ IncreasingElements/Program.cs
@@ -10,6 +10,26 @@ namespace IncreasingElements
     {
         static void Main(string[] args)
         {
+            int n = int.Parse(Console.ReadLine());
+
+            int counter = 0;
+            int previous = 0;
+            int longest = 0;
+            for (int i = 0; i < n; i++)
+            {
+                int a = int.Parse(Console.ReadLine());
+                if(a > previous)
+                {
+                    counter++;
+                }
+                else
+                {
+                    counter = 1;
+                }
+                longest = Math.Max(counter, longest);
+                previous = a;
+            }
+            Console.WriteLine(longest);
         }
     }
 }
