b4b55508a5acd0384241855301e965b2c9d0db2b
lukaszchoros <lukasz.choros@jpsfinancial.co.uk>
2019-09-19 18:52:30 +0100

    Rectangle Generator

---
+++ RectangleGenerator/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace RectangleGenerator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int m = int.Parse(Console.ReadLine());
+            int count = 0;
+            for (int left = -n; left < n; left++)
+            {
+                for (int top = -n; top < n; top++)
+                {
+                    for (int right = left+1; right <= n; right++)
+                    {
+                        for (int bottom = top+1; bottom <= n; bottom++)
+                        {
+                            int area = Math.Abs(left - right) * Math.Abs(top - bottom);
+                            if (area >= m)
+                            {
+                                Console.WriteLine("({0}, {1}) ({2}, {3}) -> {4}", left, top, right, bottom, area);
+                                count++;
+                            }
+                        }
+                    }
+                }
+            }
+            if (count == 0)
+            {
+                Console.WriteLine("No");
+            }
+        }
+    }
+}
