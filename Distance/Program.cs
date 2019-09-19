8451705b5367532cb27ba7566e4a08c77dde3358
lukaszchoros <lukasz.choros@jpsfinancial.co.uk>
2019-09-19 19:00:59 +0100

    Distance

---
+++ Distance/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Distance
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int initialSpeed = int.Parse(Console.ReadLine());
+            int firstTimeFrame = int.Parse(Console.ReadLine());
+            int secondTimeFrame = int.Parse(Console.ReadLine());
+            int thridTimeFrame = int.Parse(Console.ReadLine());
+
+            int totalTime = firstTimeFrame + secondTimeFrame + thridTimeFrame;
+
+            for (int i = 0; i < length; i++)
+            {
+
+            }
+        
+        }
+    }
+}
